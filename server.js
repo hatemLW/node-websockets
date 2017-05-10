96f2ccde0474a6d5d19d20934c74240941ee0ef5
hatemLW <hatem@logicsware.com>
2017-05-10 10:51:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,34 @@
-'use strict';
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+
+  //send the index.html file for all requests
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+http.listen(3001, function(){
+
+  console.log('listening on *:3001');
+
+});
+
+//for testing, we're just going to send data to the client every second
+setInterval( function() {
+
+  /*
+    our message we want to send to the client: in this case it's just a random
+    number that we generate on the server
+  */
+  var msg = Math.random();
+  io.emit('message', msg);
+  console.log (msg);
+
+}, 1000);
+
+/*'use strict';
 
 const express = require('express');
 const SocketServer = require('ws').Server;
@@ -29,7 +59,7 @@ function Broadcast(ws,msg)
     if(ws!=client && client.readyState === WebSocket.OPEN)
       client.send(msg);
   });
-}
+}*/
 
 /*setInterval(() => {
   wss.clients.forEach((client) => {
