61e5dbea8871208fac13c20acc8133a162e79e65
hatemLW <hatem@logicsware.com>
2017-05-10 13:09:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,25 @@ io.on('connection', function (socket) {
 
 //'use strict';
 
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello world</h1>');
+  //res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+/*
 const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
@@ -49,6 +68,7 @@ function Broadcast(ws,msg)
       client.send(msg);
   });
 }
+*/
 
 /*setInterval(() => {
   wss.clients.forEach((client) => {
