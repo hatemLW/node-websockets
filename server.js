baf6dee82b7932fb226d71845237157c65f15ed4
hatemLW <hatem@logicsware.com>
2017-05-10 14:58:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,44 +18,84 @@ io.on('connection', function (socket) {
 
 //'use strict';
  
-var express = require('express')
-  , app = express()
-  , http = require('http')
-  , server = http.createServer(app)
-  //, routes = require('./routes')
-  //, socket = require('./routes/socket.js')
-  , io = require('socket.io').listen(server);
+var ws = require("nodejs-websocket");
+var server = ws.createServer(function(conn){
+    conn.on("text", function (str) {
+        console.log("text:"+str);
+        conn.sendText(str);
+    })
+    conn.on("close", function (code, reason) {
+        console.log("close");
+    });
+    conn.on("error", function (code, reason) {
+        console.log("error");
+    });
+}).listen(3001)
 
 
-
-// Heroku config only
-/*if(process.env.PORT) {
-  io.configure(function () { 
-    io.set("transports", ["xhr-polling"]); 
-    io.set("polling duration", 10); 
-  });  
-}*/
+console.log("--WebSocket-------------");
+console.log("WebSocket address: ws://127.0.0.1:3001");
+console.log("WebSocket has started.");
+console.log("------------------------");
 
 
-// Configuration
-//var config = require('./config')(app, express);
+var http = require("http"),
+    url  = require("url"),
+    path = require("path"),
+    fs   = require("fs");
+
+http.createServer(function (req, res) {
+    var pathname=__dirname+url.parse(req.url).pathname;
+    if (path.extname(pathname)=="") {
+        pathname+="/";
+    }
+    if (pathname.charAt(pathname.length-1)=="/"){
+        pathname+="test/index.html";
+    }
+
+    path.exists(pathname,function(exists){
+        if(exists){
+            switch(path.extname(pathname)){
+                case ".html":
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    break;
+                case ".js":
+                    res.writeHead(200, {"Content-Type": "text/javascript"});
+                    break;
+                case ".css":
+                    res.writeHead(200, {"Content-Type": "text/css"});
+                    break;
+                case ".gif":
+                    res.writeHead(200, {"Content-Type": "image/gif"});
+                    break;
+                case ".jpg":
+                    res.writeHead(200, {"Content-Type": "image/jpeg"});
+                    break;
+                case ".png":
+                    res.writeHead(200, {"Content-Type": "image/png"});
+                    break;
+                default:
+                    res.writeHead(200, {"Content-Type": "application/octet-stream"});
+            }
+
+            fs.readFile(pathname,function (err,data){
+                res.end(data);
+            });
+        } else {
+            res.writeHead(404, {"Content-Type": "text/html"});
+            res.end("<h1>404 Not Found</h1>");
+        }
+    });
+
+}).listen(8080, "127.0.0.1");
 
 
-// Routes
-//app.get('/', routes.index);
-//app.get('/partials/:name', routes.partials);
+console.log("--Server----------------");
+console.log("Server address: http://127.0.0.1:8080");
+console.log("Server running... press ctrl-c to stop.");
+console.log("Server has started.");
+console.log("------------------------");
 
-// redirect all others to the index (HTML5 history)
-//app.get('*', routes.index);
-
-// Socket.io Communication
-
-io.sockets.on('connection', socket);
-
-// Start server
-var port = process.env.PORT || 5000;
-
-server.listen(port);
 /*
 const express = require('express');
 const SocketServer = require('ws').Server;
