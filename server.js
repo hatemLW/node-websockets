76eac0e2bc840686de7f6cbbadce42b5147fa03a
hatemLW <hatem@logicsware.com>
2017-05-10 16:37:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,85 +18,8 @@ io.on('connection', function (socket) {
 
 //'use strict';
  
-var ws = require("nodejs-websocket");
-var server = ws.createServer(function(conn){
-    conn.on("text", function (str) {
-        console.log("text:"+str);
-        conn.sendText(str);
-    })
-    conn.on("close", function (code, reason) {
-        console.log("close");
-    });
-    conn.on("error", function (code, reason) {
-        console.log("error");
-    });
-}).listen(3001)
 
 
-console.log("--WebSocket-------------");
-console.log("WebSocket address: ws://127.0.0.1:3001");
-console.log("WebSocket has started.");
-console.log("------------------------");
-
-
-var http = require("http"),
-    url  = require("url"),
-    path = require("path"),
-    fs   = require("fs");
-
-http.createServer(function (req, res) {
-    var pathname=__dirname+url.parse(req.url).pathname;
-    if (path.extname(pathname)=="") {
-        pathname+="/";
-    }
-    if (pathname.charAt(pathname.length-1)=="/"){
-        pathname+="test/index.html";
-    }
-
-    path.exists(pathname,function(exists){
-        if(exists){
-            switch(path.extname(pathname)){
-                case ".html":
-                    res.writeHead(200, {"Content-Type": "text/html"});
-                    break;
-                case ".js":
-                    res.writeHead(200, {"Content-Type": "text/javascript"});
-                    break;
-                case ".css":
-                    res.writeHead(200, {"Content-Type": "text/css"});
-                    break;
-                case ".gif":
-                    res.writeHead(200, {"Content-Type": "image/gif"});
-                    break;
-                case ".jpg":
-                    res.writeHead(200, {"Content-Type": "image/jpeg"});
-                    break;
-                case ".png":
-                    res.writeHead(200, {"Content-Type": "image/png"});
-                    break;
-                default:
-                    res.writeHead(200, {"Content-Type": "application/octet-stream"});
-            }
-
-            fs.readFile(pathname,function (err,data){
-                res.end(data);
-            });
-        } else {
-            res.writeHead(404, {"Content-Type": "text/html"});
-            res.end("<h1>404 Not Found</h1>");
-        }
-    });
-
-}).listen(8080, "127.0.0.1");
-
-
-console.log("--Server----------------");
-console.log("Server address: http://127.0.0.1:8080");
-console.log("Server running... press ctrl-c to stop.");
-console.log("Server has started.");
-console.log("------------------------");
-
-/*
 const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
@@ -128,7 +51,7 @@ function Broadcast(ws,msg)
       client.send(msg);
   });
 }
-*/
+
 
 /*setInterval(() => {
   wss.clients.forEach((client) => {
