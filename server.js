99f57449143c95f3e75f74cfa69da322f66aa093
hatemLW <hatem@logicsware.com>
2017-05-10 14:44:05 +0200

    Update

--- server.js
+++ server.js
@@ -17,22 +17,56 @@ io.on('connection', function (socket) {
 });*/
 
 //'use strict';
+ 
+import express from 'express';
+import bodyParser from 'body-parser';
 
-var app = require('express').createServer();
-var io = require('socket.io')(app);
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const usernames = {};
 
-app.listen(80);
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.use('/public', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', (req, res) => res.render('index'));
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+io.sockets.on('connection', (socket) => {
+  socket.on('sendchat', (data) => {
+    io.sockets.emit('updatechat', socket.username, data);
+  });
+
+  socket.on('adduser', (username) => {
+    socket.username = username;
+
+    usernames[username] = username;
+
+    socket.emit(
+      'servernotification', {
+        connected: true,
+        toSelf: true,
+        username: username
+      });
+
+    socket.broadcast.emit('servernotification', { connected: true, username: username });
+
+    io.sockets.emit('updateusers', usernames);
+  });
+
+  socket.on('disconnect', () => {
+    delete usernames[socket.username];
+
+    io.sockets.emit('updateusers', usernames);
+
+    socket.broadcast.emit('servernotification', { username: socket.username });
   });
 });
+
+server.listen(process.env.PORT || 3000);
+
 /*
 const express = require('express');
 const SocketServer = require('ws').Server;
